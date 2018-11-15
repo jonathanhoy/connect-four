8654b6927b80362bcabe8fa653fbcdeb56f2515d
jonathanhoy <jonathanhoy5@gmail.com>
2018-11-15 11:15:25 -0500

    created static win condition logic for vertical wins :smile: :smile: :smile: :smile:

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -11,8 +11,6 @@ app.columns = {
 }
 app.nextTurn = ['red'];
 
-console.log(Object.values(app.columns));
-
 // CLICK ON COLUMN TO ADD PIECE
 app.addPiece = function() {
   $('.column').on('click', function () {
@@ -26,8 +24,22 @@ app.addPiece = function() {
       $(`.column-${index} .cell-${cellNum}`).addClass(`${color}`);
       selectedColumn.push(`${color}`);
       app.changeTurn();
-      app.winCondition();
-      console.log(Object.values(app.columns));
+
+
+      // app.winCondition();
+      // console.log(Object.values(app.columns));
+
+
+
+      const counter = Object.values(app.columns);
+      let i = cellNum - 1;
+      if (counter[0].length >= 4 && counter[0][i] === counter[0][i - 1] && counter[0][i - 1] === counter[0][i - 2] && counter[0][i - 2] === counter[0][i - 3]) {
+        alert('winner');
+      }
+
+
+
+
     }
   });
 }
@@ -44,18 +56,14 @@ app.changeTurn = function() {
   }
 };
 
-app.winCondition = function() {
-  const counter = Object.values(app.columns);
-  if (counter[0].length > 1 && counter[0][0] === counter[0][1] && counter[0][0] === counter[0][2] && counter[0][0] === counter[0][3] && counter[0][1] === counter[0][2] && counter[0][1] === counter[0][3] && counter[0][2] === counter[0][3]) {
-    alert('You win!!')
-  }
-}
-
-app.winConditionVertical = function() {
-  const index = $(this).data("column");
-  const selectedColumn = app.columns[`${index}`];
-
-}
+// app.winCondition = function() {
+//   const counter = Object.values(app.columns);
+//   for (let i = 0; i <= 7; i++) {
+//     if (counter[i][i] === counter[i][i+1] && counter[i][i+1] === counter[i][i+2] && counter[i][i+2] === counter[i][i+3]) {
+//       alert('You win!!')
+//     }
+//   }
+// }
 
 app.init = function() {
   app.addPiece();
@@ -64,5 +72,4 @@ app.init = function() {
 // DOCUMENT READY
 $(function() {
   app.init();
-  app.winCondition();
 })
