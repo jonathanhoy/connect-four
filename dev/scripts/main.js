c51a290e4d4e308ff9819329f59e80dcadd442fd
jonathanhoy <jonathanhoy5@gmail.com>
2018-11-17 22:23:08 -0500

    more styling

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -14,102 +14,111 @@ app.columns = {
 }
 app.nextTurn = ['red'];
 
-app.addPiece = function() {
-  $('.column').on('click', function () {
-    const index = $(this).data("column");
-    const selectedColumn = app.columns[`${index}`];
-    if (selectedColumn.length === 6) {
-      alert('Invalid move!');
-    } else if (selectedColumn.length < 6) {
-      const color = app.nextTurn[app.nextTurn.length - 1];
-      const cellNum = selectedColumn.length + 1;
-      const i = Object.values(app.columns);
-      const x = $(this).data("array");
-      const y = cellNum - 1;
-      $(`.column-${index} .cell-${cellNum}`).append(`<div class="piece ${color} animated fadeInDownBig faster"></div>`);
-      selectedColumn.push(`${color}`);
-      app.changeTurn();
-      app.checkForWin(i, x, y, color);
-    }
-  });
-}
+// app.addPiece = function() {
+//   $('.column').on('click', function () {
+//     const index = $(this).data("column");
+//     const selectedColumn = app.columns[`${index}`];
+//     if (selectedColumn.length === 6) {
+//       alert('Invalid move!');
+//     } else if (selectedColumn.length < 6) {
+//       const color = app.nextTurn[app.nextTurn.length - 1];
+//       const cellNum = selectedColumn.length + 1;
+//       const i = Object.values(app.columns);
+//       const x = $(this).data("array");
+//       const y = cellNum - 1;
+//       $(`.column-${index} .cell-${cellNum}`).append(`<div class="piece ${color} animated fadeInDownBig faster"></div>`);
+//       selectedColumn.push(`${color}`);
+//       app.changeTurn();
+//       app.checkForWin(i, x, y, color);
+//     }
+//   });
+// }
 
-app.changeTurn = function() {
-  const currentColor = app.nextTurn[app.nextTurn.length - 1];
-  if (currentColor === 'red') {
-    app.nextTurn.push('yellow');
-    $('.turn-indicator').text(`Yellow`);
-  } else if (currentColor === 'yellow') {
-    app.nextTurn.push('red');
-    $('.turn-indicator').text(`Red`);
-  }
-};
+// app.changeTurn = function() {
+//   const currentColor = app.nextTurn[app.nextTurn.length - 1];
+//   if (currentColor === 'red') {
+//     app.nextTurn.push('yellow');
+//     $('.turn-indicator').text(`Yellow`);
+//   } else if (currentColor === 'yellow') {
+//     app.nextTurn.push('red');
+//     $('.turn-indicator').text(`Red`);
+//   }
+// };
 
-app.checkForWin = function(i, x, y, color) {
-  app.checkForVerticalWin(i, x, y, color);
-  app.checkForHorizontalWin(i, x, y, color);
-  app.checkForDiagonalWin(i, x, y, color);
-  app.checkForOtherWins(i, x, y, color);
-}
+// app.checkForWin = function(i, x, y, color) {
+//   app.checkForVerticalWin(i, x, y, color);
+//   app.checkForHorizontalWin(i, x, y, color);
+//   app.checkForDiagonalWin(i, x, y, color);
+//   app.checkForOtherWins(i, x, y, color);
+// }
 
-app.checkForVerticalWin = function(i, x, y, color) {
-  if (i[x][y] === i[x][y - 1] && i[x][y] === i[x][y - 2] && i[x][y] === i[x][y - 3]) {
-    app.alertWin(color);
-  }
-}
+// app.checkForVerticalWin = function(i, x, y, color) {
+//   if (i[x][y] === i[x][y - 1] && i[x][y] === i[x][y - 2] && i[x][y] === i[x][y - 3]) {
+//     app.alertWin(color);
+//   }
+// }
 
-app.checkForDiagonalWin = function(i, x, y, color) {
-  if (
-    // DIAGONAL WINS (TOP-LEFT TO BOTTOM-RIGHT = TL-BR, BOTTOM-LEFT TO TOP-RIGHT = BL-TR) (POS */4 = LEFT-TO-RIGHT)
-    (i[x][y] === i[x + 3][y - 3] && i[x][y] === i[x + 2][y - 2] && i[x][y] === i[x + 1][y - 1]) || // TL-BR POS 1/4
-    (i[x][y] === i[x + 2][y - 2] && i[x][y] === i[x + 1][y - 1] && i[x][y] === i[x - 1][y + 1]) || // TL-BR POS 2/4
-    (i[x][y] === i[x + 1][y - 1] && i[x][y] === i[x - 1][y + 1] && i[x][y] === i[x - 2][y + 2]) || // TL-BR POS 3/4
-    (i[x][y] === i[x + 3][y + 3] && i[x][y] === i[x + 2][y + 2] && i[x][y] === i[x + 1][y + 1]) || // BL-TR POS 1/4
-    (i[x][y] === i[x + 2][y + 2] && i[x][y] === i[x + 1][y + 1] && i[x][y] === i[x - 1][y - 1]) || // BL-TR POS 2/4
-    (i[x][y] === i[x + 1][y + 1] && i[x][y] === i[x - 1][y - 1] && i[x][y] === i[x - 2][y - 2])    // BL-TR POS 3/4
-    ) {
-    app.alertWin(color);
-  }
-}
+// app.checkForDiagonalWin = function(i, x, y, color) {
+//   if (
+//     // DIAGONAL WINS (TOP-LEFT TO BOTTOM-RIGHT = TL-BR, BOTTOM-LEFT TO TOP-RIGHT = BL-TR) (POS */4 = LEFT-TO-RIGHT)
+//     (i[x][y] === i[x + 3][y - 3] && i[x][y] === i[x + 2][y - 2] && i[x][y] === i[x + 1][y - 1]) || // TL-BR POS 1/4
+//     (i[x][y] === i[x + 2][y - 2] && i[x][y] === i[x + 1][y - 1] && i[x][y] === i[x - 1][y + 1]) || // TL-BR POS 2/4
+//     (i[x][y] === i[x + 1][y - 1] && i[x][y] === i[x - 1][y + 1] && i[x][y] === i[x - 2][y + 2]) || // TL-BR POS 3/4
+//     (i[x][y] === i[x + 3][y + 3] && i[x][y] === i[x + 2][y + 2] && i[x][y] === i[x + 1][y + 1]) || // BL-TR POS 1/4
+//     (i[x][y] === i[x + 2][y + 2] && i[x][y] === i[x + 1][y + 1] && i[x][y] === i[x - 1][y - 1]) || // BL-TR POS 2/4
+//     (i[x][y] === i[x + 1][y + 1] && i[x][y] === i[x - 1][y - 1] && i[x][y] === i[x - 2][y - 2])    // BL-TR POS 3/4
+//     ) {
+//     app.alertWin(color);
+//   }
+// }
 
-app.checkForHorizontalWin = function(i, x, y, color) {
-  if (
-    (i[x][y] === i[x + 3][y] && i[x][y] === i[x + 2][y] && i[x][y] === i[x + 1][y]) ||
-    (i[x][y] === i[x + 2][y] && i[x][y] === i[x + 1][y] && i[x][y] === i[x - 1][y]) ||
-    (i[x][y] === i[x + 1][y] && i[x][y] === i[x - 1][y] && i[x][y] === i[x - 2][y]) 
-    ) {
-      app.alertWin(color);
-    }
-  }
+// app.checkForHorizontalWin = function(i, x, y, color) {
+//   if (
+//     (i[x][y] === i[x + 3][y] && i[x][y] === i[x + 2][y] && i[x][y] === i[x + 1][y]) ||
+//     (i[x][y] === i[x + 2][y] && i[x][y] === i[x + 1][y] && i[x][y] === i[x - 1][y]) ||
+//     (i[x][y] === i[x + 1][y] && i[x][y] === i[x - 1][y] && i[x][y] === i[x - 2][y]) 
+//     ) {
+//       app.alertWin(color);
+//     }
+//   }
 
-app.checkForOtherWins = function(i, x, y, color) {
-  if (
-    (i[x][y] === i[x - 1][y + 1] && i[x][y] === i[x - 2][y + 2] && i[x][y] === i[x - 3][y + 3]) || // TL-BR POS 4/4
-    (i[x][y] === i[x - 1][y - 1] && i[x][y] === i[x - 2][y - 2] && i[x][y] === i[x - 3][y - 3]) || // BL-TR POS 4/4
-    (i[x][y] === i[x - 1][y] && i[x][y] === i[x - 2][y] && i[x][y] === i[x - 3][y]) // HORIZONTAL WIN WITH NEGATIVE X-VALUE
-    ) {
-    app.alertWin(color);
-  }
-}
+// app.checkForOtherWins = function(i, x, y, color) {
+//   if (
+//     (i[x][y] === i[x - 1][y + 1] && i[x][y] === i[x - 2][y + 2] && i[x][y] === i[x - 3][y + 3]) || // TL-BR POS 4/4
+//     (i[x][y] === i[x - 1][y - 1] && i[x][y] === i[x - 2][y - 2] && i[x][y] === i[x - 3][y - 3]) || // BL-TR POS 4/4
+//     (i[x][y] === i[x - 1][y] && i[x][y] === i[x - 2][y] && i[x][y] === i[x - 3][y]) // HORIZONTAL WIN WITH NEGATIVE X-VALUE
+//     ) {
+//     app.alertWin(color);
+//   }
+// }
 
-app.alertWin = function(color) {
-  color = color.charAt(0).toUpperCase() + color.slice(1);
-  window.setTimeout(function() {
-    swal({
-      title: `Congratulations ${color} player!`,
-      text: `Winner winner chicken dinner 🍗🍗🍗`
-    });
-  }, 650);
-}
+// app.alertWin = function(color) {
+//   color = color.charAt(0).toUpperCase() + color.slice(1);
+//   window.setTimeout(function() {
+//     swal({
+//       title: `Congratulations ${color} player!`,
+//       text: `Winner winner chicken dinner 🍗🍗🍗`
+//     });
+//   }, 650);
+// }
   
 
 app.init = function() {
   app.addPiece();
 }
 
-$('.column').hover(function() {
-  $(this).toggleClass('mouseover');
+// $('.column').hover(function() {
+//   $(this).toggleClass('mouseover');
+// });
+
+$('.instruction-button').on('click', function() {
+
 });
+
+
+$('.cell').on('click', function() {
+  $(this).css('background-color', '#333333');
+})
 
 // DOCUMENT READY
 $(function() {
